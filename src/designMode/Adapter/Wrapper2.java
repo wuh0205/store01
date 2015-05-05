95e22f9a10470ddfaac8c2325dfb435b1105b9d3
wuh0205 <eyesnull@hotmail.com>
2015-05-05 16:31:49 +0800

    修改

--- src/designMode/Adapter/Wrapper2.java
+++ src/designMode/Adapter/Wrapper2.java
@@ -18,8 +18,8 @@ public abstract class Wrapper2 implements Targetable{
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Targetable tab1=new SourceSub1();
-		Targetable tab2=new SourceSub2();
+		Wrapper2 tab1=new SourceSub1();
+		Wrapper2 tab2=new SourceSub2();
 		tab1.method1();
 		tab1.method2();
 		tab2.method1();
