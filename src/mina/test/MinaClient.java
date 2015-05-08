35af3e262192439236840a71b608602c12b9aa5b
wuh0205 <eyesnull@hotmail.com>
2015-05-08 15:57:31 +0800

    无

--- src/mina/test/MinaClient.java
+++ src/mina/test/MinaClient.java
@@ -1,18 +1,13 @@
 package mina.test;
 
-import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
-import java.nio.charset.Charset;
 
 import mina.keepAlive.ClientKeepAliveFilter;
-import mina.keepAlive.MyKeepAliveFilter;
 import mina.test.filter.CharsetCodecFactory;
 import mina.test.handler.ClientHandler;
-import mina.test.protocol.RequestMessage;
 
 import org.apache.mina.core.future.ConnectFuture;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
-import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
 import org.apache.mina.filter.logging.LoggingFilter;
 import org.apache.mina.transport.socket.nio.NioSocketConnector;
 
